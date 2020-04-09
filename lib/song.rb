cf83cc5a3f3af4d8d311d7adf61bae2f3f7daa8a
Taulant Ibraj <tibraj94@gmail.com>
2020-04-09 20:34:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,9 @@ class Song
   @@genres = []
   @@artists = []
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << genre
     @@artists << artist
